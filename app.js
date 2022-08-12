3478e444a22e90217d75aea6ce9a839f158af0ff
jinyova114 <jinyova114@icloud.com>
2022-08-12 18:32:22 +0800

    homepage prototype

---
+++ app.js
@@ -0,0 +1,19 @@
+// require packages used in the project
+const express = require('express')
+const app = express()
+const port = 3000
+const exphbs = require('express-handlebars')
+
+// setting templalte engine
+app.engine('handlebars', exphbs({ defaultLayout: 'main' }))
+app.set('view engine', 'handlebars')
+
+// routes setting 
+app.get('/', (req, res) => {
+  res.render('index')
+})
+
+// start and listen on the Express server
+app.listen(port, () => {
+  console.log(`Express is listening on localhost:${port}`)
+})
